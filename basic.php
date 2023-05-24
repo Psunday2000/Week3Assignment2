4a54ee02b22b5fa5812ee0ab54b67fd25e275bd8
Praise Chiedozie Sunday <66026084+Psunday2000@users.noreply.github.com>
2023-05-24 20:54:58 +0100

    Create basic.php

---
+++ basic.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Sunday Praise Chiedozie";
+$regnumber = "2021/HND/34246/CS";
+$state_of_origin = "Imo State";
+$project_topic = "Design and Deployment of an AI model for the detection of Hate Speech encoded in Igbo";
+
+// Output the information to the screen.
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $state_of_origin<br>";
+echo "Project topic: $project_topic<br>";
+
+?>
